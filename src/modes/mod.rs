8b25b2a92dd276f330de62608a081006ad1a911e
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-04-02 05:25:17 +0100

    modes:mod: Hand trait
    Any hand from any round mode has a chance of footstooling another hand
    from that same mode.  This trait (Hand) allows us to implement these
    semantics (as well as any other shared semantics) for each round mode.

--- src/modes/mod.rs
+++ src/modes/mod.rs
@@ -1,1 +1,13 @@
+mod pair;
 mod single;
+
+#[derive(Eq, Ord, PartialEq, PartialOrd, Debug, Copy, Clone)]
+pub enum Footstool {
+    None,
+    Half,
+    Full,
+}
+
+pub trait Hand {
+    fn footstool(&self, other: Self) -> Footstool;
+}
