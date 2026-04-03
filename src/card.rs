0c72838fd8376c6f6a9daa69a937767dfc7e9ff1
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-04-03 02:16:24 +0100

    card: PlayingCard::iter_all_deck
    Given a deck number, create an iterator over all Playing Cards in that
    deck.

--- src/card.rs
+++ src/card.rs
@@ -75,6 +75,12 @@ impl PlayingCard {
         let suit = self.suit as i64;
         (rank * 4) + suit
     }
+
+    pub fn iter_all_deck(deck: usize) -> impl Iterator<Item = Self> {
+        let deck = deck as i64;
+        ((deck * 52)..((deck + 1) * 52))
+            .filter_map(|x| PlayingCard::try_from(x).ok())
+    }
 }
 
 impl Card {
