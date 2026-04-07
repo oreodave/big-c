663e61a9e2b99972e91824a7f56059a441626d8a
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-04-07 12:35:45 +0100

    card: clippy oh clippy, clippy my beloved.

--- src/card/default.rs
+++ src/card/default.rs
@@ -1,17 +1,5 @@
 use crate::card::{Card, PlayingCard, Rank, Suit};
 
-impl Default for Rank {
-    fn default() -> Self {
-        Self::Three
-    }
-}
-
-impl Default for Suit {
-    fn default() -> Self {
-        Self::Diamond
-    }
-}
-
 impl Default for PlayingCard {
     fn default() -> Self {
         Self::new(0, Rank::default(), Suit::default())
