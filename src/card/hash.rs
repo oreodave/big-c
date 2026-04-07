1fcdce6dcb13f3ef5c5bf3b7a115ae499df1b2e9
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-04-07 12:29:49 +0100

    card:hash: implement hashing for ranks, suits, playing cards

--- src/card/hash.rs
+++ src/card/hash.rs
@@ -1,5 +1,25 @@
-use crate::card::Card;
+use crate::card::{Card, PlayingCard, Rank, Suit};
 use std::hash::{Hash, Hasher};
+
+impl Hash for Rank {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        (*self as i64).hash(state);
+    }
+}
+
+impl Hash for Suit {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        (*self as i64).hash(state);
+    }
+}
+
+impl Hash for PlayingCard {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        // NOTE: We're using the i64 conversion of card for the hash since that
+        // should generate unique numbers per card.
+        i64::from(*self).hash(state);
+    }
+}
 
 impl Hash for Card {
     fn hash<H: Hasher>(&self, state: &mut H) {
