76f180d4c61bd6af75046fdb9c74c055a22012e9
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-04-12 06:36:46 +0100

    helper: macro to generate Eq,PartialEq,PartialOrd impls for an Ord type.

--- src/helper.rs
+++ src/helper.rs
@@ -28,3 +28,26 @@ where
     I: ExactSizeIterator<Item = T>,
 {
 }
+
+/** A macro which generates Eq, PartialEq, and PartialOrd implementations for
+some given type.  These implementations are dependent on Ord already being
+implemented for that type. */
+macro_rules! impl_cmp_eq_on_ord {
+    ($type:ident) => {
+        impl PartialEq for $type {
+            fn eq(&self, other: &Self) -> bool {
+                self.cmp(other) == std::cmp::Ordering::Equal
+            }
+        }
+
+        impl Eq for $type {}
+
+        impl PartialOrd for $type {
+            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+                Some(self.cmp(other))
+            }
+        }
+    };
+}
+
+pub(crate) use impl_cmp_eq_on_ord;
