8541ee00ea7d294537e69a27cec224e2e13d5e25
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-04-16 18:39:17 +0100

    main: Simple 2 player initial deal
    Construct a deck made up of two decks of cards, shuffle it, then deal
    out 13 cards to each player.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 // permit dead code when not using clippy
 #![cfg_attr(not(clippy), allow(dead_code))]
 
+use crate::game::deck::Deck;
+
 mod card;
 mod exactsizearr;
+mod game;
 mod helper;
 mod modes;
 mod zipcartesian;
 
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::rng();
+
+    let (p1, p2, deck) = {
+        let mut p1 = Deck::new_empty();
+        let mut p2 = Deck::new_empty();
+        let mut deck = Deck::new_full(2);
+
+        deck.shuffle(&mut rng);
+        deck.deal_tail(&mut p1, 13).unwrap();
+        deck.deal_tail(&mut p2, 13).unwrap();
+        (p1, p2, deck)
+    };
+
+    println!("{}\n{}\n{}", p1, p2, deck);
 }
