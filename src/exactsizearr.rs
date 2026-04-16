674184d199d4e28312aebd97831ec898f614211f
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-04-16 11:45:41 +0100

    exactsizearr: No need for default constraint on items of the ESI input

--- src/exactsizearr.rs
+++ src/exactsizearr.rs
@@ -1,10 +1,7 @@
 /// An iterator adaptor (derived from ExactSizedIterator) which has a guaranteed
 /// compile time size, allowing for collection of an iterator into a stack
 /// allocated array.
-pub trait ExactSizedArr<I>: ExactSizeIterator<Item = I> + Sized
-where
-    I: Default,
-{
+pub trait ExactSizedArr<I>: ExactSizeIterator + Sized {
     fn into_array<const N: usize>(mut self) -> Result<[Self::Item; N], Self> {
         if self.len() < N {
             Err(self)
@@ -15,9 +12,4 @@ where
 }
 
 /// Default implementation of ExactSizedArr for any ExactSizeIterator.
-impl<T, I> ExactSizedArr<T> for I
-where
-    T: Default + Copy + Clone,
-    I: ExactSizeIterator<Item = T>,
-{
-}
+impl<I> ExactSizedArr<I> for I where I: ExactSizeIterator {}
